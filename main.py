508cd3bf8f5ac6e9bdb8726afa3bccb599b61147
Daniel Lundgren <lundgren.daniel@gmail.com>
2018-06-20 18:47:32 +0200

    Changed some dicts to lists

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import random
 from pprint import pprint
 from tkinter import *
 from collections import defaultdict
+from enum import Enum
+
+class Directions(Enum):
+	RIGHT = 0
+	UP = 1
+	LEFT = 2
+	DOWN = 3
+
+
+def string_to_direction(string):
+	if string == "left":
+		return Directions.LEFT
+	if string == "right":
+		return Directions.RIGHT
+	if string == "down":
+		return Directions.DOWN
+	if string == "up":
+		return Directions.UP
 
 # Plot settings
 plot_scale = 10
@@ -21,47 +39,53 @@ def get_rooms():
 	for file in os.listdir("rooms"):
 		with open("rooms/" + file, 'r') as f:
 			j = json.load(f)
-			rooms[j['name']] = j
+			exits = []
+			for exit in j['exits']:
+				exits.append([exit['x'], exit['y'], string_to_direction(exit['direction'])])
+			rooms[j['name']] = (j['width'], j['height'], j['type'], exits)
 	return rooms
 
-available_rooms = get_rooms()
+prefab_room_list = get_rooms()
 
 # rooms_in_map struct
 # x, y, name, rotation (0, 90, 180, 270)
 rooms_in_map = []
 
-# unsatisfied exits contains x, y, direction
-unsatisfied_exits = []
+# unconnected exits contains x, y, direction
+unconnected_exits = []
 
 
 def add_room(name, x, y):
-	if name in available_rooms.keys():
-		rooms_in_map.append({"name" : name, "x": x, "y": y, "rotation": 0})
-		for exit in available_rooms[name]['exits']:
-			unsatisfied_exits.append({"x": x + exit['x'], "y": y + exit['y'], "direction": exit['direction']})
+	if name in prefab_room_list.keys():
+		rooms_in_map.append([name, x, y, 0])
+		rx, ry, rtype, rexits = prefab_room_list[name]
+		for exit in rexits:
+			ex, ey, edir = exit
+			unconnected_exits.append([x + ex, y + ey, edir])
 
 
-def plot_rooms(rooms_in_map, available_rooms, unsatisfied_exits):
+def plot_rooms(rooms_in_map, prefab_room_list, unconnected_exits):
 	for room in rooms_in_map:
+		name, x, y, rot = room
 		# print("plotting {} at {} {}:".format(room['name'], room['x'], room['y']))
-		available_room = available_rooms[room['name']]
-		x1 = room['x']
-		y1 = room['y']
-		x2 = x1 + available_room['width']
-		y2 = y1 + available_room['height']
+		prefab_width, prefab_height, prefab_type, prefab_exits = prefab_room_list[name]
+		x2 = x + prefab_width
+		y2 = y + prefab_height
 		canvas.create_rectangle(
-			x1 * plot_scale,
-			y1 * plot_scale,
+			x * plot_scale,
+			y * plot_scale,
 			x2 * plot_scale, 
 			y2 * plot_scale, 
 			outline="blue", fill="lightgray")
-		for exit in available_room['exits']:
-			exit_x = x1 + exit['x']
-			exit_y = y1 + exit['y']
-			canvas.create_polygon(create_polygon_points(exit_x, exit_y, exit['direction']), fill="green")
-	# Plot unsatisfied exits
-	for exit in unsatisfied_exits:
-		canvas.create_polygon(create_polygon_points(exit['x'], exit['y'], exit['direction']), fill="red")
+		for exit in prefab_exits:
+			ex, ey, edir = exit
+			absolute_ex = x + ex
+			absolute_ey = y + ey
+			canvas.create_polygon(create_polygon_points(absolute_ex, absolute_ey, edir), fill="green")
+	# Plot unconnected exits
+	for exit in unconnected_exits:
+		ex, ey, edir = exit
+		canvas.create_polygon(create_polygon_points(ex, ey, edir), fill="red")
 
 
 def create_polygon_points(x, y, direction):
@@ -70,16 +94,14 @@ def create_polygon_points(x, y, direction):
 	cell_mid_x = x + half_cell_height
 	cell_mid_y = y + half_cell_height
 	points.append([(cell_mid_x) * plot_scale, (cell_mid_y) * plot_scale])
-	if direction in ("up", "left"):
+	if direction in (Directions.UP, Directions.LEFT):
 		points.append([x * plot_scale, y * plot_scale])
-	if direction in ("down", "left"):
+	if direction in (Directions.DOWN, Directions.LEFT):
 		points.append([x * plot_scale, (y + 1) * plot_scale])
-	if direction in ("up", "right"):
+	if direction in (Directions.UP, Directions.RIGHT):
 		points.append([(x + 1) * plot_scale, y * plot_scale])
-	if direction in ("down", "right"):
+	if direction in (Directions.DOWN, Directions.RIGHT):
 		points.append([(x + 1) * plot_scale, (y + 1) * plot_scale])
-	
-	# print(points)
 	return points
 
 
@@ -88,46 +110,53 @@ def draw_grid(grid_size, spacing):
 		canvas.create_line(x * spacing, 0, x * spacing, grid_size * spacing, dash=(3,1))
 		canvas.create_line(0, x * spacing, grid_size * spacing, (x * spacing), dash=(3,1))
 
+
 def opposite_direction(direction):
-	if direction == "up":
-		return "down"
-	if direction == "down":
-		return "up"
-	if direction == "left":
-		return "right"
-	if direction == "right":
-		return "left"
+	if direction == Directions.UP:
+		return Directions.DOWN
+	if direction == Directions.DOWN:
+		return Directions.UP
+	if direction == Directions.LEFT:
+		return Directions.RIGHT
+	if direction == Directions.RIGHT:
+		return Directions.LEFT
 	return "INVALID"
 
 def direction_offset(direction):
-	if direction == "right":
+	if direction == Directions.RIGHT:
 		return 1, 0
-	if direction == "left":
+	if direction == Directions.LEFT:
 		return -1, 0
-	if direction == "up":
+	if direction == Directions.UP:
 		return 0, -1
-	if direction == "down":
+	if direction == Directions.DOWN:
 		return 0, 1
 
 
 def add_room_to_random_exit():
-	unsatisfied_exit = random.choice(unsatisfied_exits)
-	print("Unsatisfied exit found at {} {} direction: {}"
-		.format(unsatisfied_exit['x'], unsatisfied_exit['y'], unsatisfied_exit['direction']))
+	unconnected_exit = random.choice(unconnected_exits)
+	uce_x, uce_y, uce_dir = unconnected_exit 
+	print("unconnected exit found at {} {} direction: {}"
+		.format(uce_x, uce_y, uce_dir))
 	# Find a room that has an exit that fits
-	for room in available_rooms.values():
-		for e in room['exits']:
-			ue_x = unsatisfied_exit['x']
-			ue_y = unsatisfied_exit['y']
-			ue_d = unsatisfied_exit['direction']
-			re_offset_x = e['x']
-			re_offset_y = e['y']
-			dir_offset_x, dir_offset_y = direction_offset(ue_d)
-			if e["direction"] == opposite_direction(ue_d):
-				print("Found exit to match the unsatisfied exit")
-				add_room(room['name'], 
-					ue_x + re_offset_x + dir_offset_x, 
-					ue_y + re_offset_y + dir_offset_y)
+	for prefab_name, prefab_room in prefab_room_list.items():
+
+		prefab_type, prefab_width, prefab_height, prefab_exits = prefab_room
+		for prefab_exit in prefab_exits:
+			print(prefab_exit)
+			e_x, e_y, e_dir = prefab_exit
+			dir_offset_x, dir_offset_y = direction_offset(uce_dir)
+			if e_dir == opposite_direction(uce_dir):
+				print("Found exit to match the unconnected exit")
+				new_room_x = uce_x + e_x + dir_offset_x
+				new_room_y = uce_y + e_y + dir_offset_y
+				add_room(prefab_name, new_room_x, new_room_y)
+				# Remove unconnected exit and the corresponding from the newly added room
+				unconnected_exits.remove(unconnected_exit)
+				# unconnected_exits.remove(e)
+				print(unconnected_exit)
+				print(prefab_exit)
+				print(unconnected_exits)
 
 add_room("mid1", 10, 10)
 add_room_to_random_exit()
@@ -136,7 +165,7 @@ add_room_to_random_exit()
 # Append random room to any available exits
 
 
-plot_rooms(rooms_in_map, available_rooms, unsatisfied_exits)
+plot_rooms(rooms_in_map, prefab_room_list, unconnected_exits)
 draw_grid(grid_size, plot_scale)
 
 canvas.pack()
