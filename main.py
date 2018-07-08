a763e7c98923b227132d0fc4b41632fe6cf31554
Daniel Lundgren <lundgren.daniel@gmail.com>
2018-07-08 11:55:47 +0200

    Started moving the room class to own file.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 from pprint import pprint
 from tkinter import *
 from collections import defaultdict
-from enum import Enum
 from rect import *
+from room import *
 
 # Plot settings
 plot_scale = 10
@@ -24,54 +24,6 @@ canvas = Canvas(master,
                 height=plot_scale * grid_size)
 
 
-class Directions(Enum):
-    RIGHT = 0
-    UP = 1
-    LEFT = 2
-    DOWN = 3
-
-
-class RoomExit():
-    """
-    Relative to the prefab room or absolute position in map
-    """
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
-        self.direction = direction
-
-
-class PrefabRoom:
-    def __init__(self, prefab_json):
-        self.name = prefab_json['name']
-        self.width = prefab_json['width']
-        self.height = prefab_json['height']
-        self.type = prefab_json['type']
-        self.exits = []
-        for exit in prefab_json['exits']:
-            x = exit['x']
-            y = exit['y']
-            direction_str = exit['direction']
-            direction = string_to_direction(direction_str)
-            self.exits.append(RoomExit(x,
-                                       self.height - exit['y'] - 1,
-                                       direction))
-
-
-class Room:
-    def __init__(self, name, x, y, rotation, exits):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.rotation = rotation
-        self.exits = []
-        # Add offset and rotation
-        for exit in exits:
-            self.exits.append(RoomExit(x + exit.x,
-                                       y + exit.y,
-                                       exit.direction))
-
-
 def get_rooms():
     rooms = defaultdict(list)
     for file in os.listdir("rooms"):
@@ -79,17 +31,6 @@ def get_rooms():
             prefab_room = PrefabRoom(json.load(f))
             rooms[prefab_room.name] = prefab_room
     return rooms
-
-
-def string_to_direction(string):
-    if string == "left":
-        return Directions.LEFT
-    if string == "right":
-        return Directions.RIGHT
-    if string == "down":
-        return Directions.DOWN
-    if string == "up":
-        return Directions.UP
 
 
 def add_room(room_list, prefab_room_list, name, x, y):
---
+++ room.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+
+class Directions(Enum):
+    RIGHT = 0
+    UP = 1
+    LEFT = 2
+    DOWN = 3
+
+
+class Room:
+    def __init__(self, name, x, y, rotation, exits):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.rotation = rotation
+        self.exits = []
+        # Add offset and rotation
+        for exit in exits:
+            self.exits.append(RoomExit(x + exit.x,
+                                       y + exit.y,
+                                       exit.direction))
+
+
+class RoomExit():
+    """
+    Relative to the prefab room or absolute position in map
+    """
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+
+
+class PrefabRoom:
+    def __init__(self, prefab_json):
+        self.name = prefab_json['name']
+        self.width = prefab_json['width']
+        self.height = prefab_json['height']
+        self.type = prefab_json['type']
+        self.exits = []
+        for exit in prefab_json['exits']:
+            x = exit['x']
+            y = exit['y']
+            direction_str = exit['direction']
+            direction = string_to_direction(direction_str)
+            self.exits.append(RoomExit(x,
+                                       self.height - exit['y'] - 1,
+                                       direction))
+
+
+def string_to_direction(string):
+    if string == "left":
+        return Directions.LEFT
+    if string == "right":
+        return Directions.RIGHT
+    if string == "down":
+        return Directions.DOWN
+    if string == "up":
+        return Directions.UP
