4aa38dc3440b5646493da9247f85bf56ec17cb01
Daniel Lundgren <lundgren.daniel@gmail.com>
2018-07-08 11:42:40 +0200

    Added unit test for rect overlap

--- rect.py
+++ rect.py
@@ -6,9 +6,9 @@ class Rect:
         self.y2 = y + height
 
     def overlaps(self, rect):
-        print("Check overlap_x", self.x, self.x2, rect.x, rect.x2)
-        print("Check overlap_y", self.y, self.y2, rect.y, rect.y2)
-        # TODO: Check ranges?
+        """
+        Since this is used in a discrete system, 0-2 does not overlap with 2-3.
+        """
         x_overlap = (self.x < rect.x < self.x2 or
                      self.x < rect.x2 < self.x2 or
                      rect.x < self.x < rect.x2 or
@@ -20,7 +20,5 @@ class Rect:
                      rect.y < self.y2 < rect.y2)
 
         if x_overlap and y_overlap:
-            print("OVERLAPPING")
             return True
-        print("NOT OVERLAPPING")
         return False
---
+++ rect_unittest.py
@@ -0,0 +1,32 @@
+import unittest
+from rect import *
+
+
+class TestRect(unittest.TestCase):
+
+    def test_rect_overlaps(self):
+        a = Rect(10, 10, 2, 2)
+        b = Rect(11, 11, 2, 2)
+        self.assertTrue(a.overlaps(b))
+        self.assertTrue(b.overlaps(a))
+
+    def test_rect_does_not_overlaps_when_adjacent(self):
+        a = Rect(10, 10, 2, 2)
+        b = Rect(10, 12, 2, 2)
+        c = Rect(12, 10, 2, 2)
+        d = Rect(8, 10, 2, 2)
+        e = Rect(10, 8, 2, 2)
+        self.assertFalse(a.overlaps(b))
+        self.assertFalse(a.overlaps(c))
+        self.assertFalse(a.overlaps(d))
+        self.assertFalse(a.overlaps(e))
+
+    def test_rect_overlaps_when_one_rect_contains_the_other(self):
+        a = Rect(10, 10, 2, 2)
+        b = Rect(9, 9, 4, 4)
+        self.assertTrue(a.overlaps(b))
+        self.assertTrue(b.overlaps(a))
+
+
+if __name__ == '__main__':
+    unittest.main()
